81de990eb6d890e1fd422c30f8707080e075c74c
luizacariani <63241702+luizacariani@users.noreply.github.com>
2021-08-22 13:38:28 -0300

    Add files via upload

---
+++ au2/main.c
@@ -0,0 +1,29 @@
+/*
+ * GccApplication2.c
+ *
+ * Created: 22/08/2021 10:37:38
+ * Author : luiza
+ * github:
+ */ 
+
+#include <avr/io.h>
+#include <avr/delay.h>
+
+char estadoPino;
+
+main(){
+	DDRB |= (1 << DDB4);
+	estadoPino = PINC4;
+
+	char LEDestadoPino(char pino){
+		pino = PINC4;
+		return pino;
+	}
+
+	while(1){
+		PORTB |= (1 << 4);
+		_delay_ms(2000);
+		PORTB &= (1 << 4);
+		_delay_ms(2000);
+	}
+};
