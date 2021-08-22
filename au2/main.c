d8f2c7fea577ffe127ae021ad89514676dfbcc3a
luizacariani <63241702+luizacariani@users.noreply.github.com>
2021-08-22 13:53:13 -0300

    Add files via upload

---
+++ au2/main.c
@@ -0,0 +1,31 @@
+/*
+ * GccApplication2.c
+ *
+ * Created: 22/08/2021 10:37:38
+ * Author : luiza
+ * github:https://github.com/luizacariani/Programacaodehardware2021/tree/main/au2
+ *Programação que acende o led quando o pino é abaixado.
+ *O parametro estPino é o estado do pino, o parametro LEDestPino é a relação entre o pino e o LED
+ */ 
+
+#include <avr/io.h>
+#include <avr/delay.h>
+
+char estadoPino;
+
+main(){
+	DDRB |= (1 << DDB4);
+	estPino = PINC4;
+
+	char LEDestPino(char pino){
+		pino = PINC4;
+		return pino;
+	}
+
+	while(1){
+		PORTB |= (1 << 4);
+		_delay_ms(2000);
+		PORTB &= (1 << 4);
+		_delay_ms(2000);
+	}
+};
